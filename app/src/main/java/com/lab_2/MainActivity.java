b6bb6890462f7fbd33faa4ae1ec3a3a33c68a758
gvozd <Sachok1394@mail.ru>
2016-10-20 23:36:34 +0300

    add UI

--- app/src/main/java/com/lab_2/MainActivity.java
+++ app/src/main/java/com/lab_2/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private Intent NotificationIntent;
+    private Intent LaunchIntent;
     private Context context;
 
     @Override
@@ -46,4 +47,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onCreateMeat(View view){
+
+    }
+
+    public void getAllMeat(View view){
+
+    }
+
+
 }
