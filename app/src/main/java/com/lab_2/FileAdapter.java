3a8ff2ccbd48d4cf359e5fe563deff264f5af68b
gvozd <Sachok1394@mail.ru>
2016-11-07 14:58:13 +0300

    add fileAdapter and createExportDialog

---
+++ app/src/main/java/com/lab_2/FileAdapter.java
@@ -0,0 +1,52 @@
+package com.lab_2;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by Александр on 07.11.2016.
+ */
+
+public class FileAdapter extends ArrayAdapter<File> implements View.OnClickListener {
+    public String deltaPath;
+    public FileAdapter(Context context, List<File> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Nullable
+    @Override
+    public File getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView view = (TextView) super.getView(position, convertView, parent);
+        view.setOnClickListener(this);
+        File file = getItem(position);
+        view.setText(file.getName());
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        TextView textView = (TextView) view;//сделать приписку к пути
+        deltaPath = textView.getText().toString();//проверить что тут лежит
+        view.setBackgroundColor(Color.GRAY);
+    }
+
+    public String getDeltaPath() {
+        return deltaPath;
+    }
+}
---
+++ app/src/main/java/com/lab_2/OpenFileDialog.java
@@ -0,0 +1,59 @@
+package com.lab_2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Environment;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Александр on 07.11.2016.
+ */
+
+public class OpenFileDialog extends AlertDialog.Builder {
+    public static String FILE_SEPAATOR ="/";
+    private String currentPath = Environment.getExternalStorageDirectory().getPath();
+    private List<File> files = new ArrayList<File>();
+    private FileAdapter FileAdapter;
+    private List<Meeting> ExportData;
+
+    public OpenFileDialog(Context context,List<Meeting> exp) {
+        super(context);
+        this.ExportData =exp;
+        files.addAll(getFiles(currentPath));
+        FileAdapter = new FileAdapter(context,getFiles(currentPath));
+        setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String s = FileAdapter.getDeltaPath()+FILE_SEPAATOR;//тут та папка которую вызерет пользователь
+                currentPath = currentPath+s;
+            }
+        })
+                .setNegativeButton(android.R.string.cancel, null);
+        setAdapter(FileAdapter,null);
+    }
+    private List<File> getFiles(String directoryPath){
+        File directory = new File(directoryPath);
+        List<File> fileList = Arrays.asList(directory.listFiles());
+        Collections.sort(fileList, new Comparator<File>() {
+            @Override
+            public int compare(File file, File file2) {
+                if (file.isDirectory() && file2.isFile())
+                    return -1;
+                else if (file.isFile() && file2.isDirectory())
+                    return 1;
+                else
+                    return file.getPath().compareTo(file2.getPath());
+            }
+        });
+        return fileList;
+    }
+
+
+}
