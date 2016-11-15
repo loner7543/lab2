01a1d066a20e8381ea893a9627d97e5fa5211e51
gvozd <Sachok1394@mail.ru>
2016-11-16 00:54:25 +0300

    add login activitty and delete focus on main list , change createMeeeting activity

--- app/src/main/java/com/lab_2/LoginActivity.java
+++ app/src/main/java/com/lab_2/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.lab_2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends Activity {
 
     private static final String TAG = "SignInActivity";
 
@@ -27,11 +37,86 @@ public class LoginActivity extends AppCompatActivity{
         loginEditText = (EditText) findViewById(R.id.login);
         passEditText = (EditText) findViewById(R.id.password);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-    public void onSignIn(View view){
-
+    public void onSignIn(View view) {
+        signIn();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null) {
+            onAuthSuccess(mAuth.getCurrentUser());
+        }
+    }
+
+    private void signIn() {
+        Log.d(TAG, "signIn");
+        if (!validateForm()) {
+            return;
+        }
+        String email = loginEditText.getText().toString();
+        String password = passEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        //hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+        writeNewUser(user.getUid(), username, user.getEmail());
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(loginEditText.getText().toString())) {
+            loginEditText.setError("Required");
+            result = false;
+        } else {
+            loginEditText.setError(null);
+        }
+
+        if (TextUtils.isEmpty(passEditText.getText().toString())) {
+            passEditText.setError("Required");
+            result = false;
+        } else {
+            passEditText.setError(null);
+        }
+
+        return result;
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
 }
 
---
+++ app/src/main/java/com/lab_2/User.java
@@ -0,0 +1,34 @@
+package com.lab_2;
+
+/**
+ * Created by Александр on 16.11.2016.
+ */
+
+public class User {
+    public String username;
+    public String email;
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
