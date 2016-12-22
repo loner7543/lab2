cb957a12d603f7275c0faba3528666e58bbe5a84
gvozd <Sachok1394@mail.ru>
2016-12-22 13:46:10 +0300

    fix export

--- app/src/main/java/com/lab_2/OpenFileDialog.java
+++ app/src/main/java/com/lab_2/OpenFileDialog.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.lab_2.adapters.FileAdapter;
 import com.lab_2.domain.Meeting;
@@ -34,36 +36,42 @@ public class OpenFileDialog extends AlertDialog.Builder {
 
     public OpenFileDialog(Context context, final List<Meeting> exp) {
         super(context);
-        this.ExportData =exp;
-        files.addAll(getFiles(currentPath));
-        FileAdapter = new FileAdapter(context,getFiles(currentPath));
-        setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                exportedData = new ArrayList<String[]>();
-                // готовим данные для экспорта
-                for (Meeting m:exp){
-                    exportedData.add(new String[]{m.toString()});
-                }
-                String s = FILE_SEPARATOR+FileAdapter.getDeltaPath()+FILE_SEPARATOR;//тут та папка которую вызерет пользователь
-                currentPath = currentPath+s;
-                File resExport = new File(currentPath,exportedFileName);//создает директорию а не файл !!!!!
-                try {
-                    if(resExport.createNewFile())
-                    {
-                        CSVWriter csvWriter = new CSVWriter(new FileWriter(resExport));
-                        csvWriter.writeAll(exportedData);
-                        csvWriter.close();
-
-
+        try {
+            this.ExportData =exp;
+            files.addAll(getFiles(currentPath));
+            FileAdapter = new FileAdapter(context,getFiles(currentPath));
+            setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    exportedData = new ArrayList<String[]>();
+                    // готовим данные для экспорта
+                    for (Meeting m:exp){
+                        exportedData.add(new String[]{m.toString()});
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    String s = FILE_SEPARATOR+FileAdapter.getDeltaPath()+FILE_SEPARATOR;//тут та папка которую вызерет пользователь
+                    currentPath = currentPath+s;
+                    File resExport = new File(currentPath,exportedFileName);//создает директорию а не файл !!!!!
+                    try {
+                        if(resExport.createNewFile())
+                        {
+                            CSVWriter csvWriter = new CSVWriter(new FileWriter(resExport));
+                            csvWriter.writeAll(exportedData);
+                            csvWriter.close();
+
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        })
-                .setNegativeButton(android.R.string.cancel, null);
-        setAdapter(FileAdapter,null);
+            })
+                    .setNegativeButton(android.R.string.cancel, null);
+            setAdapter(FileAdapter,null);
+        }
+        catch (Exception e){
+            Log.d("OpenFileDialog","Ошибка экспорта");
+        }
+
     }
     private List<File> getFiles(String directoryPath){
         File directory = new File(directoryPath);
